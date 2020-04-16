70b962e6599a3a4105cc3453c4884124b876de01
Terence Yak <terenceyak@istox.com>
2020-04-16 21:12:49 +0800

    Add test to showcase pass

---
+++ test/expectEvent.test.js
@@ -0,0 +1,24 @@
+const BN = web3.utils.BN;
+const { expect } = require('chai').use(require('chai-bn')(BN));
+const { expectEvent } = require('@openzeppelin/test-helpers');
+const EventEmitter = artifacts.require('EventEmitter');
+const IndirectEventEmitter = artifacts.require('IndirectEventEmitter');
+
+contract('IndirectExpectEvent', function() {
+    beforeEach(async function () {
+        this.emitter = await EventEmitter.new();
+        this.indirectEmitter = await IndirectEventEmitter.new();
+    });
+
+    it('should emit indirect uint', async function () {
+        const value = new BN(1);
+        const { tx } = await this.emitter.emitUintIndirectly(value, this.indirectEmitter.address);
+        await expectEvent.inTransaction(tx, IndirectEventEmitter, 'IndirectUint', {
+            value
+        });
+    });
+
+    it('should pass', function () {
+        expect('11').to.be.bignumber.equal(new BN(11));
+    });
+});
